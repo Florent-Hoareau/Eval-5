d50631c6f2ea14d31478d7de6aefcc1f06f9cafd
Hoareau Florent <104675603+Florent-Hoareau@users.noreply.github.com>
2022-12-12 10:12:55 +0100

    Structure et config des fichiers ok
    Renommage des .html en .njk
    Création des templates language avec njk.
    Configuration des fichiers : .eleventy, package.json

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addPassthroughCopy('css')
+	eleventyConfig.addPassthroughCopy('images')
+	eleventyConfig.addPassthroughCopy('js')
+	eleventyConfig.addPassthroughCopy('src')
+
 	return {
+		passthroughFileCopy: true,
 		dir: {
 			input: "src",
             output: "public"
